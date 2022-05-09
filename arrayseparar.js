b12e75b93563bb4d8f04893c4c935bae439a6a40
ssarinha <102757853+ssarinha@users.noreply.github.com>
2022-05-09 15:30:00 -0300

    Add files via upload

---
+++ arrayseparar.js
@@ -0,0 +1,8 @@
+const chamada = ['Ray', 'Ruan', 'Xablau', 'MaridasDores', 'Felipe', 'Danilo', 'Daneil', 'Erico']
+console.log (chamada)
+
+let grupo1 = chamada.slice (0, chamada.length/2)
+let grupo2 = chamada.slice (0, chamada.length/2)
+
+console.log (grupo1)
+console.log (grupo2)
---
+++ arraysubstituir.js
@@ -0,0 +1,4 @@
+let chamada = ['Leo', 'Davi', 'VK', 'Lai']
+chamada.splice(1, 2, 'Nicolly')
+
+console.log(chamada)
